1f00f727dedfa9c885d2d9703fd787389b874d7a
karuvsentor <karuvsentor@gmail.com>
2021-04-18 12:36:43 +0200

    add userProfile

--- teammate/src/components/AppRouter/AppRouter.js
+++ teammate/src/components/AppRouter/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react' 
 import {Route, Switch } from 'react-router-dom'
 import Home from '../Home/Home'
+import UserProfile from '../UserProfile/UserProfile'
 
 
 const AppRouter = () => {
@@ -8,6 +9,7 @@ const AppRouter = () => {
     return(
         <Switch>
             <Route exact path='/' component={Home} />
+            <Route exact path='/userProfile' component={UserProfile} />
         </Switch>
     )
 
---
+++ teammate/src/components/UserProfile/UserProfile.js
@@ -0,0 +1,16 @@
+import React from 'react' 
+import './UserProfile.css'
+
+
+const UserProfile = () => {
+
+    return (
+        <div className="UserProfile">
+                <p>User Profile</p>
+        </div>
+    )
+
+
+}
+
+export default UserProfile
